5c938f7ba8384913e14128f177e482b33d6275de
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-04-19 22:26:40 -0700

    Created CrudService as parent interface to reduces duplication

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,12 +2,7 @@ package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
 
-import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO,String> {
 
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void deleteById(String username);
 }
